989620ccb8e41d4ea8a31ba038e499c5e97f5fd5
devAltss <altoss11@gmail.com>
2021-09-25 23:16:47 +0300

    added file media, end project

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,15 @@ $(function () {
     $('.rightside-menu').addClass('rightside-menu--close');
   });
 
+  $('.header__btn-menu').on('click', function () {
+    $('.menu').toggleClass('menu--open');
+  });
+
+  if($(window).width() < 651) {
+    $('.work-path__item--measuring').appendTo($('.work-path__items-box'));
+  }
+
+
   $('.top__slider').slick({
     dots: true,
     arrows: false,
@@ -19,6 +28,53 @@ $(function () {
     slidesToScroll: 10,
     dots: true,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1700,
+        settings: {
+          slidesToShow: 8,
+          slidesToScroll: 8
+        }
+      },
+      {
+        breakpoint: 1511,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5
+        }
+      },
+      {
+        breakpoint: 1201,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4
+        }
+      },
+      {
+        breakpoint: 841,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 541,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 376,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 
   $('.article-slider__box').slick({
@@ -33,8 +89,8 @@ $(function () {
     }
   });
 
-  
-  
+
+
 
 
 })
